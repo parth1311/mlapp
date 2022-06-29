a8ef6af4ffe20a53a0a55a1372805c6b32fe7b1a
parth1311 <rsp131196@gmail.com>
2022-06-29 23:12:07 +0530

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='First MLApp using CookieCutter',
+    author='Parthasarathy R',
+    license='MIT',
+)
